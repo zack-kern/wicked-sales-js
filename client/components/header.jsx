f14d054ecbee84c5a6ec49fdbd1ea6a864bde534
Zack Kern <zkkern@gmail.com>
2020-11-04 23:37:03 -0800

    Completed view-product-details-back-end || Zack Kern

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -5,7 +5,7 @@ function Header() {
     <div className="Header container">
       <div className="row">
         <iframe src="https://giphy.com/embed/l49JMVDvP8D38LHwI" width="60" height="60" frameBorder="0"></iframe>
-        <h1 className="col-sm d-flex justify-content-center">Wicked SALEz</h1>
+        <h1 className="col-sm d-flex justify-content-center">~~~ Wicked SALEz ~~~~</h1>
         <iframe src="https://giphy.com/embed/l49JMVDvP8D38LHwI" width="60" height="60" frameBorder="0"></iframe>
       </div>
     </div>
